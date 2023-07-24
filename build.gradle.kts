6fa05560dabd238578d89d513f250ed6ff586ed2
Nick Tchayka <nick@booster.cloud>
2023-07-24 16:24:28 +0100

    Update project so it retrieves content

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,50 +1,42 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.9.0"
-    application
+  kotlin("jvm") version "1.9.0"
+  application
 }
 
 group = "com.theagilemonkeys"
+
 version = "1.0-SNAPSHOT"
 
-allprojects {
-    repositories {
-        maven { url = uri("https://jitpack.io") }
-    }
-}
+allprojects { repositories { maven { url = uri("https://jitpack.io") } } }
 
-repositories {
-    mavenCentral()
-}
+repositories { mavenCentral() }
 
 dependencies {
-    implementation("com.github.theam.ellmental:core:main-SNAPSHOT")
-    implementation("com.github.theam.ellmental:semanticsearch:main-SNAPSHOT")
-    implementation("com.github.theam.ellmental:vectorstore:main-SNAPSHOT")
-    implementation("com.github.theam.ellmental:embeddingsmodel:main-SNAPSHOT")
-    implementation("com.aallam.openai:openai-client:3.3.0")
-    testImplementation(kotlin("test"))
+  implementation("com.github.theam.ellmental:core:main-SNAPSHOT")
+  implementation("com.github.theam.ellmental:semanticsearch:main-SNAPSHOT")
+  implementation("com.github.theam.ellmental:vectorstore:main-SNAPSHOT")
+  implementation("com.github.theam.ellmental:embeddingsmodel:main-SNAPSHOT")
+
+  implementation("com.aallam.openai:openai-client:3.3.0")
+  implementation("io.ktor:ktor-client-core:2.3.2")
+  implementation("io.ktor:ktor-client-okhttp:2.3.2")
+  implementation("com.squareup.okhttp3:okhttp:4.10.0")
+  implementation(platform("org.http4k:http4k-bom:5.4.0.0"))
+  implementation("org.http4k:http4k-client-okhttp")
+
+  testImplementation(kotlin("test"))
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
+tasks.test { useJUnitPlatform() }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
-}
+tasks.withType<KotlinCompile> { kotlinOptions.jvmTarget = "17" }
 
 tasks.withType<KotlinCompile>().configureEach {
-    kotlinOptions {
-        freeCompilerArgs = freeCompilerArgs + "-Xcontext-receivers"
-    }
+  kotlinOptions { freeCompilerArgs = freeCompilerArgs + "-Xcontext-receivers" }
 }
 
-tasks.named<JavaExec>("run") {
-    standardInput = System.`in`
-}
+tasks.named<JavaExec>("run") { standardInput = System.`in` }
 
-application {
-    mainClass.set("MainKt")
-}
+application { mainClass.set("MainKt") }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,79 +6,70 @@ import com.theagilemonkeys.ellmental.vectorstore.pinecone.PineconeVectorStore
 import kotlinx.coroutines.runBlocking
 
 fun embeddingsModel(): OpenAIEmbeddingsModel {
-    val openaiToken = System.getenv("OPEN_AI_API_KEY")
-    check(openaiToken != null) { "OPEN_AI_API_KEY environment variable is not set" }
-    with(OpenAI(token = openaiToken)) {
-        return OpenAIEmbeddingsModel()
-    }
+  val openaiToken = System.getenv("OPEN_AI_API_KEY")
+  check(openaiToken != null) { "OPEN_AI_API_KEY environment variable is not set" }
+  with(OpenAI(token = openaiToken)) {
+    return OpenAIEmbeddingsModel()
+  }
 }
 
 fun vectorStore(): PineconeVectorStore {
-    val pineconeToken = System.getenv("PINECONE_API_KEY")
-    check (pineconeToken != null) { "PINECONE_API_KEY environment variable is not set" }
-    val pineconeUrl = System.getenv("PINECONE_URL")
-    check (pineconeUrl != null) { "PINECONE_URL environment variable is not set" }
-    return PineconeVectorStore(apiKey = pineconeToken, url = pineconeUrl)
+  val pineconeToken = System.getenv("PINECONE_API_KEY")
+  check(pineconeToken != null) { "PINECONE_API_KEY environment variable is not set" }
+  val pineconeUrl = System.getenv("PINECONE_URL")
+  check(pineconeUrl != null) { "PINECONE_URL environment variable is not set" }
+  return PineconeVectorStore(apiKey = pineconeToken, url = pineconeUrl)
 }
 
 fun semanticSearch(): SemanticSearch {
-    val embeddingsModel = embeddingsModel()
-    val vectorStore = vectorStore()
-    with(embeddingsModel) {
-        with(vectorStore) {
-            return SemanticSearch()
-        }
+  val embeddingsModel = embeddingsModel()
+  val vectorStore = vectorStore()
+  with(embeddingsModel) {
+    with(vectorStore) {
+      return SemanticSearch()
     }
+  }
 }
 
-data class Note(
-    val id: Int,
-    val content: String
-)
+data class Note(val id: Int, val content: String)
 
 suspend fun learn(semanticSearch: SemanticSearch, note: Note) {
-    semanticSearch.learn(SearchInput(listOf(note.content)))
+  semanticSearch.learn(SearchInput(listOf(note.content)))
 }
 
 suspend fun search(semanticSearch: SemanticSearch, query: String): List<String> {
-    return semanticSearch.search(query).entries.map {
-        it.id.value
-    }
+  return semanticSearch.search(query).entries.map { it.content }
 }
 
 fun main() = runBlocking {
-    val semanticSearch = semanticSearch()
-    println("Available commands: learn, search, quit\n")
-    var shouldQuit = false
-    while (!shouldQuit) {
-        print("> ")
-        when (readln()) {
-            "help" -> {
-                println("Available commands: learn, search, quit")
-            }
-            "learn" -> {
-                println("Enter note content:")
-                val txt = readln()
-                val note = Note(
-                    id = 1,
-                    content = txt
-                )
-                learn(semanticSearch, note)
-            }
-            "search" -> {
-                println("Enter query:")
-                val query = readln()
-                val results = search(semanticSearch, query)
-                println(results)
-            }
-            "quit" -> {
-                println("Bye!")
-                shouldQuit = true
-            }
-            else -> {
-                println("Invalid command")
-            }
-        }
+  val semanticSearch = semanticSearch()
+  println("Available commands: learn, search, quit\n")
+  var shouldQuit = false
+  while (!shouldQuit) {
+    print("> ")
+    when (readln()) {
+      "help" -> {
+        println("Available commands: learn, search, quit")
+      }
+      "learn" -> {
+        println("Enter note content:")
+        val txt = readln()
+        val note = Note(id = 1, content = txt)
+        learn(semanticSearch, note)
+      }
+      "search" -> {
+        println("Enter query:")
+        val query = readln()
+        val results = search(semanticSearch, query)
+        println(results)
+      }
+      "quit" -> {
+        println("Bye!")
+        shouldQuit = true
+      }
+      else -> {
+        println("Invalid command")
+      }
     }
+  }
 }
-
